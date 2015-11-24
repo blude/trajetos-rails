765a10c316031e11a12cb099e93924a7e40aed4c
Saulo Pratti <saulo@wine.com.br>
2015-11-24 19:27:46 -0200

    correcao de rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  resources :locations
   resources :trips
   resources :routes
-  resources :routes
+
   root 'pages#index'
 
-  resources :routes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
