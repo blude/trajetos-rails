c03c68c4aae08104ee3c308c5cd2df532bff5a7a
Saulo Pratti <sppratti@gmail.com>
2015-11-27 00:11:07 -0200

    corrigido associacao

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ActiveRecord::Base
-  belongs_to :point
+  has_one :point
 
   validates :lat, presence: true, numericality: { less_than_or_equal_to: 90.0, greater_than_or_equal_to: -90.0 }
   validates :lng, presence: true, numericality: { less_than_or_equal_to: 180.0, greater_than_or_equal_to: -180.0 }
